dab5b19da4c8f418a77d608ef8f2a4368a022d9e
Stephen Gordon <gordoste@iinet.net.au>
2025-02-11 22:46:53 +1100

    ASoC: Add ezsound-6x8iso driver
    Signed-off-by: Stephen Gordon <gordoste@iinet.net.au>

--- sound/soc/bcm/ezsound-6x8iso.c
+++ sound/soc/bcm/ezsound-6x8iso.c
@@ -49,23 +49,24 @@ static int snd_ezsound6x8_hw_params(struct snd_pcm_substream *substream,
 				       struct snd_pcm_hw_params *params)
 {
 	struct snd_soc_pcm_runtime *rtd = substream->private_data;
-	struct snd_soc_dai *cpu_dai = snd_soc_rtd_to_cpu(rtd, 0);
-	struct snd_soc_dai *codec_dai = snd_soc_rtd_to_codec(rtd, 0);
-	int ret;
+	struct snd_soc_dai *codec_dai;
+	int i, ret;
 
-	switch (params_rate(params)){
-		case 96000:
-			ret = snd_soc_dai_set_sysclk(codec_dai, 0, 24576000, SND_SOC_CLOCK_IN);
-			if (ret && ret != -ENOTSUPP)
-				return ret;
-			break;
-		default:
-			return -EINVAL;
+	for_each_rtd_codec_dais(rtd, i, codec_dai) {
+		switch (params_rate(params)){
+			case 96000:
+				ret = snd_soc_dai_set_sysclk(codec_dai, 0, 24576000, SND_SOC_CLOCK_IN);
+				if (ret && ret != -ENOTSUPP)
+					return ret;
+				break;
+			default:
+				return -EINVAL;
+		}
+
+		ret = snd_soc_dai_set_tdm_slot(codec_dai, 0, 0, 2, 32);
+		if (ret && ret != -ENOTSUPP)
+			return ret;
 	}
-
-	ret = snd_soc_dai_set_tdm_slot(codec_dai, 0, 0, 2, 32);
-	if (ret && ret != -ENOTSUPP)
-		return ret;
 	return 0;
 }
 
@@ -77,34 +78,49 @@ static struct snd_soc_ops snd_ezsound6x8_ops = {
 
 static int ezsound6x8_dai_init(struct snd_soc_pcm_runtime *rtd)
 {
-	struct snd_soc_dai *cpu_dai = snd_soc_rtd_to_cpu(rtd, 0);
-	struct snd_soc_dai *codec_dai = snd_soc_rtd_to_codec(rtd, 0);
-	int ret;
+	struct snd_soc_dai *codec_dai;
+	int i, ret;
 
-	ret = snd_soc_dai_set_sysclk(codec_dai, 0, 24576000, SND_SOC_CLOCK_IN);
-	if (ret && ret != -ENOTSUPP)
-		return ret;
+	for_each_rtd_codec_dais(rtd, i, codec_dai) {
+		ret = snd_soc_dai_set_sysclk(codec_dai, 0, 24576000, SND_SOC_CLOCK_IN);
+		if (ret && ret != -ENOTSUPP)
+			return ret;
 
-	ret = snd_soc_dai_set_tdm_slot(codec_dai, 0, 0, 2, 32);
-	if (ret && ret != -ENOTSUPP)
-		return ret;
-
+		ret = snd_soc_dai_set_tdm_slot(codec_dai, 0, 0, 2, 32);
+		if (ret && ret != -ENOTSUPP)
+			return ret;
+	}
 	return 0;
 }
 
-SND_SOC_DAILINK_DEFS(ezsound6x8,
+SND_SOC_DAILINK_DEFS(ezsound6x8_dac,
 	DAILINK_COMP_ARRAY(COMP_CPU("bcm2835-i2s.0")),
 	DAILINK_COMP_ARRAY(COMP_CODEC("pcm3168a.1-0045", "pcm3168a-dac")),
 	DAILINK_COMP_ARRAY(COMP_PLATFORM("bcm2835-i2s.0")));
 
+SND_SOC_DAILINK_DEFS(ezsound6x8_adc,
+	DAILINK_COMP_ARRAY(COMP_CPU("bcm2835-i2s.0")),
+	DAILINK_COMP_ARRAY(COMP_CODEC("pcm3168a.1-0045", "pcm3168a-adc")),
+	DAILINK_COMP_ARRAY(COMP_PLATFORM("bcm2835-i2s.0")));
+
 static struct snd_soc_dai_link ezsound6x8_dai[] = {
 	{
-		.name = "ezsound6x8",
-		.stream_name = "ezsound6x8-hifi",
+		.name = "ezsound6x8-dac",
+		.stream_name = "ez6x8-playback",
 		.ops = &snd_ezsound6x8_ops,
 		.init = ezsound6x8_dai_init,
-		.dai_fmt = SND_SOC_DAIFMT_CBM_CFM|SND_SOC_DAIFMT_I2S|SND_SOC_DAIFMT_NB_NF,
-		SND_SOC_DAILINK_REG(ezsound6x8),
+		.dai_fmt = SND_SOC_DAIFMT_CBM_CFM | SND_SOC_DAIFMT_I2S \
+			| SND_SOC_DAIFMT_NB_NF,
+		SND_SOC_DAILINK_REG(ezsound6x8_dac),
+	},
+	{
+		.name = "ezsound6x8-adc",
+		.stream_name = "ez6x8-capture",
+		.ops = &snd_ezsound6x8_ops,
+		.init = ezsound6x8_dai_init,
+		.dai_fmt = SND_SOC_DAIFMT_CBM_CFM | SND_SOC_DAIFMT_I2S \
+			| SND_SOC_DAIFMT_NB_NF,
+		SND_SOC_DAILINK_REG(ezsound6x8_adc),
 	},
 };
 
@@ -128,15 +144,18 @@ static int ezsound6x8_probe(struct platform_device *pdev)
 	card->dev = &pdev->dev;
 
 	if (pdev->dev.of_node) {
-		struct snd_soc_dai_link *dai = &ezsound6x8_dai[0];
+		struct snd_soc_dai_link *dai;
 		struct device_node *i2s_node = of_parse_phandle(pdev->dev.of_node,
 								"i2s-controller", 0);
 
 		if (i2s_node) {
-			dai->cpus->dai_name = NULL;
-			dai->cpus->of_node = i2s_node;
-			dai->platforms->name = NULL;
-			dai->platforms->of_node = i2s_node;
+			for (int i = 0; i < snd_soc_ezsound6x8.num_links; i++) {
+				dai = &(ezsound6x8_dai[i]);
+				dai->cpus->dai_name = NULL;
+				dai->cpus->of_node = i2s_node;
+				dai->platforms->name = NULL;
+				dai->platforms->of_node = i2s_node;
+			}
 			of_node_put(i2s_node);
 		} else
 			if (!dai->cpus->of_node) {
